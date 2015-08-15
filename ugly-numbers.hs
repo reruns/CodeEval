87c6bdf5b98effe23c46143beade91280b25a96f
Garrett <graffitisouls@gmail.com>
2015-08-15 05:47:14 -0400

    some minor changes to ugly-numbers

--- ugly-numbers.hs
+++ ugly-numbers.hs
@@ -1,3 +1,5 @@
+--this isn't fast enough
+--memoization is a good choice
 import System.Environment
 
 ugly x = any (\y -> x `mod` y == 0) [2,3,5,7]
@@ -13,11 +15,8 @@ apply (x:y:xs) (Shift:os) = apply ((x << y):xs) os
 apply (x:xs) (Add:os) = x + (apply xs os)
 apply (x:xs) (Sub:os) = x - (apply xs os)
 
---I think the reason test cases are failing is this:
---ops has to be mostly evaluated before we can use elements of the result
---which takes up a huge amount of memory for longer strings
 ops 0 = [[]]
-ops x = let os = ops (x-1) in (map (Add:) os) ++ (map (Sub:) os) ++ (map (Shift:) os)
+ops n = concat [ map (x:) (ops (n-1)) | x <- [Add, Sub, Shift]]
 
 solve x = let digits = map (\x -> read [x] :: Int) x
               os = ops (length x - 1)
